6564de73da6097d54d5a83b4befb29f731ffe559
Liam O'Brien <86622061+ldobrien1234@users.noreply.github.com>
2021-08-18 10:58:54 -0700

    Update cell_neuralnet.py

--- cell_neuralnet.py
+++ cell_neuralnet.py
@@ -4,15 +4,19 @@ import matplotlib.pyplot as plt
 import numpy as np
 import torch
 import torch.nn as nn
-from torch.autograd.functional import jacobian
 
 import dgl
 from torch.utils.data import DataLoader, random_split
-from GCNLayers import GCNLayer1, GNODEFunc, ODEBlock, compute_MAPE
+from GCNLayers import GCNLayer2, GNODEFunc, ODEBlock, compute_MAPE
 from data_classes import GNODEData
 
+batch_size=100
 
-batch_size=1000
+train_epochs = 5
+test_epochs = 2 
+
+#final time of the numerical integrator
+t_f = 10
 
 #lets us use our computer's gpu if it's available
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
@@ -38,7 +42,10 @@ torch.manual_seed(0) #seed for random numbers
 
 
 #get dataset from file
-dataset = GNODEData("non_random_dataset.txt")
+dataset = GNODEData("Cell123_Features.txt", "Cell123_TimeSeries.txt")
+num_eval = dataset.num_eval
+nCell = dataset.nCell
+
 
 #split into train and test
 size_train = round((dataset.__len__() / 10)*8)
@@ -60,44 +67,47 @@ test_loader = DataLoader(test, batch_size=batch_size, shuffle=True)
 #                               torch.tensor([0,1,2, 1,2,2]))})
 
 #complete graph
-g_top = dgl.heterograph({ ('cell','interacts','cell'):
+g_top = dgl.heterograph({('cell','interacts','cell'):
                               (torch.tensor([0,1,2, 0,0, 1,1, 2,2]),
                               torch.tensor([0,1,2, 1,2, 0,2, 0,1]))})
-    
-#Now let's create a normalization tensor (based on the degree of
-#each node) to improve learning
-degs = g_top.in_degrees().float()
-norm = torch.pow(degs, -0.5) #each degree to the power of (-1/2)
-norm[torch.isinf(norm)] = 0
-# add to dgl.Graph in order for the norm to be accessible at training time
-g_top.ndata['norm'] = norm.unsqueeze(1).to(device)
-
-
 
 
 #Now we can define the model################################################
 #dynamics defined by two GCN layers
-gnn = nn.Sequential(GCNLayer1(g=g_top, in_feats=3, out_feats=40, 
+gnn = nn.Sequential(GCNLayer1(g=g_top, batch_size=batch_size, nCell=nCell,
+                              in_feats=20, out_feats=50, 
                               dropout=0.5, activation=nn.LeakyReLU()),
-                    GCNLayer1(g=g_top, in_feats=40, out_feats=3, 
+                    GCNLayer1(g=g_top, batch_size=batch_size, nCell=nCell,
+                              in_feats=50, out_feats=20, 
                             dropout=None, activation=None)
                   ).to(device)
                    
 gnode_func = GNODEFunc(gnn)
 
 
-#ODEBlock class let's us use an ode solver to find our input at a later time
-gnode = ODEBlock(gnode_func, method = 'rk4', atol=1e-3, rtol=1e-4,
-                 adjoint=False)
+#create a tensor of times to evaluate our numerical integrator
+eval_int = t_f / num_eval
+tspan = torch.empty(num_eval+1)
+for i in range(num_eval+1):
+    tspan[i] = i*eval_int
+    
 
-model = gnode
+#ODEBlock class lets us use an ode solver to find our input at a later time
+#gnode outputs a tensor of dimension (num_eval, nCell, no. features)
+gnode = ODEBlock(gnode_func, tspan=tspan, method = 'rk4', atol=1e-3, 
+                 rtol=1e-4, adjoint=True)
 
-opt = torch.optim.Adam(model.parameters(), weight_decay=5e-4)
+#model outputs a tensor of dimension (num_eval, nCell, no. features)
+model = nn.Sequential(GCNLayer1(g=g_top, batch_size=batch_size, nCell=nCell,
+                                in_feats=3, out_feats=20,
+                                dropout=0.5, activation=nn.LeakyReLU()),
+                      gnode,
+                      nn.Linear(20,3))
+                      
+
+opt = torch.optim.Adam(model.parameters())
 criterion = nn.MSELoss()
 
-
-train_epochs = 8
-test_epochs = 3 
 
 steps = 0
 train_y_plt = []
@@ -106,33 +116,32 @@ train_mape_plt = []
 for epoch in range(train_epochs):
     model.train()
     
-    for i, (features, targets) in enumerate(train_loader):
-            
-        features = features.to(device)
-        targets = targets.to(device)
+    for (features, targets) in train_loader:
+          
+        features = features.to(device) #dimension (batch_size, nCell, no. features)
+        targets = targets.to(device) #dimension (batch_size, nCell, num_eval, no. features)
         
-        outputs = torch.zeros(batch_size, 3, 3)
-        for k, feature in enumerate(features):
-            
-            y_pred = model(feature)
-            
-            outputs[k, ::, ::]= y_pred
+        h_final = model(features) #dimension (num_eval+1, batch_size, nCell, no. features)
         
-        loss = criterion(targets, outputs)
-        MAPE = compute_MAPE(targets, outputs).item()
+        #make h_final have dimension (batch_size, nCell, num_eval, no. features)
+        #now directly comparable with targets
+        h_final = h_final[1:,::,::,::].permute(1,2,0,3)
+        
+        loss = criterion(targets, h_final)
+        MAPE = compute_MAPE(targets, h_final)
         
         opt.zero_grad()
         loss.backward()
         opt.step()
-        
+            
         steps += 1
-        
+            
         print("train loss = ", loss.item())
-        print("train MAPE = ", MAPE)
+        print("train MAPE = ", MAPE.item())
         print("steps = ", steps)
         train_y_plt.append(loss.item())
-        train_mape_plt.append(MAPE)
-        
+        train_mape_plt.append(MAPE.item())
+                
         
 
 
@@ -145,30 +154,33 @@ for epoch in range(test_epochs):
     
     with torch.no_grad():
         
-        for i, (features, targets) in enumerate(test_loader):
+        for (features, targets) in test_loader:
             
-            features = features.to(device)
-            targets = targets.to(device)
+            features = features.to(device) #dimension (batch_size, nCell, no. features)
+            targets = targets.to(device) #dimension (batch_size, nCell, num_eval, no. features)
+             
+            h_final = model(features) #dimension (num_eval+1, nCell, no. features)
+                 
+            #make h_final have dimension (batch_size, nCell, num_eval, no. features)
+            #now directly comparable with targets
+            h_final = h_final[1:,::,::,::].permute(1,2,0,3)
             
-            outputs = torch.zeros(batch_size, 3, 3)
-            for k, feature in enumerate(features):
-                
-                y_pred = model(feature)
-                
-                outputs[k, ::, ::]= y_pred
-            
-            loss = criterion(targets, outputs)
-            MAPE = compute_MAPE(targets, outputs).item()
-            
+            loss = criterion(targets, h_final)
+            MAPE = compute_MAPE(targets, h_final)
+
             steps += 1
-            
+           
             print("test loss = ", loss.item())
-            print("test MAPE = ", MAPE)
+            print("test MAPE = ", MAPE.item())
             print("steps = ", steps)
             test_y_plt.append(loss.item())
-            test_mape_plt.append(MAPE)
+            test_mape_plt.append(MAPE.item())
             
 
+
+
+#save the trained model in a file
+torch.save(model.state_dict(), "GNODE_Model.pt")
 
 
 
@@ -180,6 +192,7 @@ test_y_plt = np.array(test_y_plt)
 plt.title('Loss')
 plt.plot(train_x_plt, train_y_plt, 'blue', label='training loss')
 plt.plot(test_x_plt, test_y_plt, 'orange', label='test loss')
+plt.legend()
 
 plt.figure()
 
@@ -191,25 +204,11 @@ test_mape_plt = np.array(test_mape_plt)
 plt.title('MAPE')
 plt.plot(train_x_plt, train_mape_plt, 'blue', label= 'training MAPE')
 plt.plot(test_x_plt, test_mape_plt, 'orange', label = 'test MAPE')
-    
+plt.legend()
     
 #save the training loss in a file
-text_file = open("sample8.txt", "w")
+text_file = open("gnode_loss.txt", "w")
 for loss, MAPE in zip(train_y_plt, train_mape_plt):
     message = "loss = " + str(loss) +", MAPE = " + str(MAPE) + "\n"
     text_file.write(message)
 text_file.close()
-
-#Lastly, we want our model to learn causal dependencies####################
-
-#we trained our model, and now we want to find the derivative of the
-#dynamics of each output with respect to each input
-total_J = torch.zeros(3,3,3,3)
-for i, (features, targets) in enumerate(test_loader):
-    for k, feature in enumerate(features):
-        #the jacobian gives causal dependencies at a given observation
-        J_gnn = jacobian(gnn.eval(), feature)
-        #summing the jacobian is more revealing of average causal dependencies
-        total_J += J_gnn
-    
-print(J_gnn)
